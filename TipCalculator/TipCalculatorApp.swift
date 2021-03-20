150b33767dfc27ca36d310077d0c8644b33306a1
Subhrajyoti <subha.ionic3@gmail.com>
2021-03-20 20:17:30 +0530

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Subhrajyoti Chakraborty on 20/03/21.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
